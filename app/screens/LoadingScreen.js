ea41fd87b8a779d752dd52de1bd362d71cf0d0eb
huangkun101230 <huangkun101230@gmail.com>
2020-09-16 15:51:20 +1200

    title component gene

--- app/screens/LoadingScreen.js
+++ app/screens/LoadingScreen.js
@@ -1,22 +1,13 @@
-/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import colors from '../configs/colors';
+import Title from './../components/title';
 
 class LoadingScreen extends React.Component {
   render() {
     return (
       <View style={cusStyles.container}>
-        <View style={{flexDirection: 'row'}}>
-          <View style={cusStyles.divider} />
-          <Text style={cusStyles.title}>
-            Assignment
-            <Text style={{fontWeight: '300', color: colors.primary}}>
-              Manager
-            </Text>
-          </Text>
-          <View style={cusStyles.divider} />
-        </View>
+        <Title />
       </View>
     );
   }
@@ -28,18 +19,6 @@ const cusStyles = StyleSheet.create({
     backgroundColor: colors.backgroundColor,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  divider: {
-    backgroundColor: colors.secondary,
-    height: 1,
-    flex: 1,
-    alignSelf: 'center',
-  },
-  title: {
-    fontSize: 28,
-    fontWeight: '800',
-    color: colors.black,
-    paddingHorizontal: 34,
   },
 });
 
